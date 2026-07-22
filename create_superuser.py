b4d9cb1c40bc9affc3ab748f1cea604f72fd8bd8
Cyggnuz <apeg1999@gmail.com>
2026-07-21 22:26:30 -0400

    Configurar user

--- create_superuser.py
+++ create_superuser.py
@@ -1,21 +1,23 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xikers_site.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xikers_site.settings")
 django.setup()
 
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-# Modifica con tus datos para ingresar al admin
-USERNAME = 'cyggnuz'
-EMAIL = 'apeg1999@gmail.com'
-PASSWORD = 'Churrascowaton1'  # 👈 Cambia esto por la clave que quieras usar
+USERNAME = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+EMAIL = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+PASSWORD = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
 if not User.objects.filter(username=USERNAME).exists():
-    print(f"Creando superusuario {USERNAME}...")
-    User.objects.create_superuser(username=USERNAME, email=EMAIL, password=PASSWORD)
-    print("¡Superusuario creado con éxito!")
+    User.objects.create_superuser(
+        username=USERNAME,
+        email=EMAIL,
+        password=PASSWORD
+    )
+    print("Superusuario creado.")
 else:
-    print(f"El superusuario {USERNAME} ya existe.")
+    print("El superusuario ya existe.")
